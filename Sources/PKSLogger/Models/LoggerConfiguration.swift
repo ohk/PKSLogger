b9d3c359041f7a542d759713cedbd5894977a345
Omer Hamid Kamisli <me@ohkamisli.dev>
2024-06-09 17:51:44 -0700

    feat: Refactor LoggerConfiguration.swift and add defaultGlobalConfiguration property

--- Sources/PKSLogger/Models/LoggerConfiguration.swift
+++ Sources/PKSLogger/Models/LoggerConfiguration.swift
@@ -50,3 +50,59 @@ extension LoggerConfiguration {
         self.category = category
     }
 }
+
+extension LoggerConfiguration {
+    /// Initializes a new `LoggerConfiguration` object.
+    ///
+    ///
+    /// - Parameters:
+    ///  - storeLogs: A boolean value that indicates whether logs should be stored or not.
+    ///  - logLevel: An integer value that indicates the log level.
+    ///  - hideLogs: A boolean value that indicates whether logs should be hidden or not.
+    ///  - subsystem: A string value that indicates the subsystem.
+    ///  - category: A string value that indicates the category.
+    ///  - Returns: A new `LoggerConfiguration` object.
+    ///
+    static func defaultConfiguration(subsystem: String, category: String, logLevel: OSLogType) -> LoggerConfiguration {
+        switch logLevel {
+        case .default, .info, .debug, .error, .fault:
+            return LoggerConfiguration(
+                storeLogs: true,
+                logLevel: logLevel,
+                hideLogs: false,
+                subsystem: subsystem,
+                category: category
+            )
+        case .production:
+            return LoggerConfiguration(
+                storeLogs: false,
+                logLevel: .production,
+                hideLogs: true,
+                subsystem: subsystem,
+                category: category
+            )
+        default:
+            return LoggerConfiguration(
+                storeLogs: false,
+                logLevel: .default,
+                hideLogs: true,
+                subsystem: subsystem,
+                category: category
+            )
+        }
+    }
+
+    /// Static property that returns the default global configuration.
+    ///
+    /// - Returns: A new `LoggerConfiguration` object.
+    ///
+    static var defaultGlobalConfiguration: LoggerConfiguration {
+        return LoggerConfiguration(
+            storeLogs: true,
+            logLevel: .debug,
+            hideLogs: false,
+            subsystem: "com.poikus.foundation.logger",
+            category: "GLOBAL"
+        )
+    }
+}
