94d4f8965b87a23ce730c6e66a73cd5c020c87e6
Omer Hamid Kamisli <me@ohkamisli.dev>
2024-06-09 18:57:27 -0700

    feat: Add LoggerDetailView for displaying logger details

---
+++ Sources/PKSLogger/Views/LoggerDetailView/LoggerDetailView.swift
@@ -0,0 +1,37 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Ömer Hamid Kamışlı on 6/9/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    @State var logger: PKSLogger
+    
+    var body: some View {
+        List {
+            SettingsView(sectionName: "Settings", logger: logger)
+            
+            Section("Logs") {
+                ForEach(logger.logs, id: \.self) { log in
+                    Text(log)
+                }
+            }
+
+        }
+    }
+}
+
+#Preview {
+    let logger = PKSLogger(.defaultGlobalConfiguration)
+    logger.logLevel = .default
+    logger.info("Test 1")
+    logger.info("Test 2")
+    logger.info("Test 3")
+    logger.info("Test 4")
+    logger.warning("Test 5")
+    logger.fault("Test 6")
+    return SwiftUIView(logger: logger)
+}
