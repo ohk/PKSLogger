035d11cf6402afa043f12efec835b5c9db27d73e
Omer Hamid Kamisli <me@ohkamisli.dev>
2024-06-09 17:36:40 -0700

    Refactor PKSLogger to be a public class with an id property

--- Sources/PKSLogger/PKSLogger.swift
+++ Sources/PKSLogger/PKSLogger.swift
@@ -1,3 +1,15 @@
-struct PKSLogger {
+//
+//  PKSLogger.swift
+//  PKSLogger
+//
+//  Created by Ömer Hamid Kamışlı on 6/5/24.
+//
+
+import OSLog
+
+public class PKSLogger: ObservableObject, Identifiable {
+    /// The unique identifier of the logger.
+    public var id: String
+    
     
 }
