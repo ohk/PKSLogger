ade868172fd8f5d5949949dfbab63dc2d1cd0066
Omer Hamid Kamisli <me@ohkamisli.dev>
2024-06-09 18:28:57 -0700

    feat: Add getLoggerConfiguration method to PKSLoggerManager for retrieving logger configuration

--- Sources/PKSLogger/PKSLoggerManager.swift
+++ Sources/PKSLogger/PKSLoggerManager.swift
@@ -59,4 +59,54 @@ public class PKSLoggerManager: ObservableObject {
             systemLogger.info("PKSLogger | MANAGER | Global Configuration Changes started to track")
     }
     
+    /// This function will be returned PKSLogger object based on subsystem and category.
+    ///
+    /// This function will try to get the logger from the dictionary. If the logger is not found, it will create a new logger with the given subsystem and category.
+    /// ```swift
+    /// let logger = PKSLoggerManager.shared.getLogger(subsystem: "com.example", category: "example")
+    /// logger.debug("Hello, World!")
+    /// ```
+    /// - Parameters:
+    ///   - subsystem: A string value that indicates the subsystem.
+    ///   - category: A string value that indicates the category.
+    /// - Returns: A `PKSLogger` object.
+    public func getLogger(subsystem: String, category: String) -> PKSLogger {
+        let key = "\(subsystem)-\(category)"
+        if let logger = loggers[key] {
+            systemLogger.info("PKSLogger | MANAGER | Logger found in the dictionary. Returning the existing logger.")
+            return logger
+        } else {
+            systemLogger.info("PKSLogger | MANAGER | Logger not found in the dictionary. Creating a new logger.")
+            let configuration = getLoggerConfiguration(subsystem: subsystem, category: category)
+            let logger = PKSLogger(configuration)
+            loggers[key] = logger
+            return logger
+        }
+    }
+    
+    /// Get logger configuration based on subsystem and category.
+    ///
+    /// This function will try to load the logger settings from the `UserDefaults` with the key `PKSLoggerConfiguration-\(subsystem)-\(category)`.
+    /// If the logger settings are not found, this function will return the default logger configuration.
+    /// Default logger configuration is based on the global logger configuration.
+    /// - Parameters:
+    ///  - subsystem: A string value that indicates the subsystem.
+    ///  - category: A string value that indicates the category.
+    /// - Returns: A `LoggerConfiguration` object that holds the logger settings.
+    ///
+    private func getLoggerConfiguration(subsystem: String, category: String) -> LoggerConfiguration {
+        if let configuration = userDefaultsManager.loadLoggerSettings(subsystem: subsystem, category: category) {
+            systemLogger.info("PKSLogger | MANAGER | Logger Configuration found in UserDefaults. Returning the existing configuration.")
+            return configuration
+        } else {
+            systemLogger.info("PKSLogger | MANAGER | Logger Configuration not found in UserDefaults. Returning the default configuration.")
+            return LoggerConfiguration(
+                storeLogs: loggerGlobalConfiguration.storeLogs,
+                logLevel: loggerGlobalConfiguration.logLevel,
+                hideLogs: loggerGlobalConfiguration.hideLogs,
+                subsystem: subsystem,
+                category: category
+            )
+        }
+    }
 }
